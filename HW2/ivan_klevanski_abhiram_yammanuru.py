060b2526d5ecbff0ad9c5a0c598209550fd9f131
ibarnstormer <48729319+ibarnstormer@users.noreply.github.com>
2024-09-23 20:09:45 -0400

    Initial commit

---
+++ HW2/ivan_klevanski_abhiram_yammanuru.py
@@ -0,0 +1,32 @@
+"""
+CS 548 Team Project 2
+
+Authors: Ivan Klevanski Abhiram Yammanuru
+
+
+Notes:
+
+Place all files (csvs) in the same
+directory as the source file
+
+"""
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import os
+
+from scipy import stats
+
+abs_path = os.path.dirname(os.path.abspath(__file__))
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
