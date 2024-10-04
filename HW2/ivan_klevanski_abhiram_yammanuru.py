432307b88660903749bfeb26f1426588c50f5019
ibarnstormer <48729319+ibarnstormer@users.noreply.github.com>
2024-10-04 19:34:05 -0400

    Preliminary model training / testing

--- HW2/ivan_klevanski_abhiram_yammanuru.py
+++ HW2/ivan_klevanski_abhiram_yammanuru.py
@@ -16,9 +16,15 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
+import imblearn.under_sampling as im_us
+import sklearn.ensemble as sk_e
+import sklearn.metrics as sk_m
+import sklearn.model_selection as sk_ms
 import sklearn.pipeline as sk_p
 import sklearn.preprocessing as sk_pp
+import sklearn.tree as sk_t
 import warnings
+import xgboost as xgb
 
 from scipy import stats
 
@@ -29,6 +35,7 @@ abs_path = os.path.dirname(os.path.abspath(__file__))
 # Plots can take time to render, set to false to speed up execution
 plot_fraud_to_feature = False
 plot_base_eda = False
+plot_cm = True
 
 def init_dataset():
     """
@@ -73,6 +80,8 @@ def data_preprocessing(df: pd.DataFrame):
     Preprocesses the dataset's features 
     """
 
+    print("======Data Preprocessing======")
+
     # One Hot Encoding:
     df_non_numeric_col = df.select_dtypes(include=object).columns
     df_numeric = df.select_dtypes(include=np.number)
@@ -85,13 +94,29 @@ def data_preprocessing(df: pd.DataFrame):
 
     df = df_numeric
 
+    # Undersampling
+    feat_df = df.drop(columns=["fraud_bool"])
+    label_df = df["fraud_bool"]
+
+    sampler = im_us.NearMiss()
+    feat_df_fit, label_df_fit = sampler.fit_resample(feat_df, label_df)
+
+    feat_df_fit["fraud_bool"] = label_df_fit
+
+    df = feat_df_fit.sample(frac=1).reset_index(drop=True)
+
+    # TODO: maybe also add SMOTE for some oversampling
+
     # Other preprocessing
 
+    print("Done.")
     return df
 
 
 
 def EDA(df: pd.DataFrame):
+
+    print("======EDA======")
 
     # Get dataset-specific information
 
@@ -177,6 +202,89 @@ def EDA(df: pd.DataFrame):
     """
 
     df.drop(columns=["device_fraud_count"], inplace=True)
+    print("Done.")
+
+def evaluate_model(y, y_hat, name: str, print_cm: bool):
+    """
+    Computes various model evaluation metrics
+
+    **y**: actual labels
+    **y_hat**: predictions
+    """
+
+    cm = sk_m.confusion_matrix(y, y_hat)
+
+    if print_cm:
+        sns.heatmap(cm, annot=True, fmt='d', cmap="viridis")
+        plt.title("{} Confusion Matrix".format(name))
+        plt.xlabel("Predicted")
+        plt.ylabel("True")
+        plt.show()
+
+
+    tn, fp, fn, tp = cm.ravel()
+    p = tp / (tp + fp)
+    r = tp / (tp + fn)
+
+    acc = sk_m.accuracy_score(y, y_hat) * 100
+    f1 = sk_m.f1_score(y, y_hat)
+    mcc = sk_m.matthews_corrcoef(y, y_hat)
+
+    print("{}: Accuracy: {:.4f}, Precision: {:.4f}, Recall: {:.4f}, F1-Score: {:.4f}, MCC: {:.4f}".format(name, acc, p, r, f1, mcc))
+
+
+def model_decision_tree(df: pd.DataFrame):
+    """
+    TODO: Implement K-fold CV
+    """
+    print("======Decision Tree======")
+
+    feat_df = df.drop(columns=["fraud_bool"])
+    X_train, X_test, y_train, y_test = sk_ms.train_test_split(feat_df, df["fraud_bool"], train_size=0.8, test_size=0.2)
+
+    clf = sk_t.DecisionTreeClassifier()
+
+    clf.fit(X=X_train, y=y_train)
+    y_pred = clf.predict(X_test)
+
+    evaluate_model(y_test, y_pred, "Decision Tree", plot_cm)
+    print("Done.")
+
+
+def model_random_forest(df: pd.DataFrame):
+    """
+    TODO: Implement K-fold CV
+    """
+    print("======Random Forest======")
+
+    feat_df = df.drop(columns=["fraud_bool"])
+    X_train, X_test, y_train, y_test = sk_ms.train_test_split(feat_df, df["fraud_bool"], train_size=0.8, test_size=0.2)
+
+    clf = sk_e.RandomForestClassifier()
+
+    clf.fit(X=X_train, y=y_train)
+    y_pred = clf.predict(X_test)
+
+    evaluate_model(y_test, y_pred, "Random Forest", plot_cm)
+    print("Done.")
+
+
+def model_xgboost(df: pd.DataFrame):
+    """
+    TODO: Implement K-fold CV
+    """
+    print("======XGBoost======")
+
+    feat_df = df.drop(columns=["fraud_bool"])
+    X_train, X_test, y_train, y_test = sk_ms.train_test_split(feat_df, df["fraud_bool"], train_size=0.8, test_size=0.2)
+
+    clf = xgb.XGBClassifier()
+
+    clf.fit(X=X_train, y=y_train)
+    y_pred = clf.predict(X_test)
+
+    evaluate_model(y_test, y_pred, "XGBoost", plot_cm)
+    print("Done.")
 
 
 def main():
@@ -190,6 +298,12 @@ def main():
     EDA(df)
 
     df = data_preprocessing(df)
+
+    # Model training
+
+    model_random_forest(df)
+    model_decision_tree(df)
+    model_xgboost(df)
 
     pass
 
